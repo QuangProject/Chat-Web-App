83e56e01cdb46da3ea422a2af6144a9d287c9e59
quangndgcc200030 <91590378+quangndgcc200030@users.noreply.github.com>
2023-06-06 08:09:11 +0700

    fix homepage

--- src/app/middlewares/AuthMiddleware.js
+++ src/app/middlewares/AuthMiddleware.js
@@ -7,6 +7,7 @@ class AuthMiddleware {
             // get all receipts of user
             const receipts = await Receipt.getReceiptOfUser(req.session.user.user_id)
             const receiptsObj = receipts.rows
+            const numberReceipts = receipts.rowCount
             for (let i = 0; i < receiptsObj.length; i++) {
                 const date1 = new Date();
                 const date2 = new Date(receipts.rows[i].created_at);
@@ -33,6 +34,7 @@ class AuthMiddleware {
                 receiptsObj[i].time_receipt = time_receipt
             }
             res.locals.notifications = receiptsObj
+            res.locals.numberReceipts = numberReceipts
 
             res.locals.user = req.session.user
             next()
@@ -42,8 +44,40 @@ class AuthMiddleware {
     }
 
     // User or admin can access
-    freedom(req, res, next) {
+    async freedom(req, res, next) {
         if (req.session.user) {
+            // get all receipts of user
+            const receipts = await Receipt.getReceiptOfUser(req.session.user.user_id)
+            const receiptsObj = receipts.rows
+            const numberReceipts = receipts.rowCount
+            for (let i = 0; i < receiptsObj.length; i++) {
+                const date1 = new Date();
+                const date2 = new Date(receipts.rows[i].created_at);
+
+                const differenceInMilliseconds = date1.getTime() - date2.getTime();
+                const differenceInSeconds = differenceInMilliseconds / 1000;
+                const differenceInMinutes = differenceInSeconds / 60;
+                const differenceInHours = differenceInMinutes / 60;
+                const differenceInDays = differenceInHours / 24;
+
+                var time_receipt = ''
+                if (Math.floor(differenceInDays) > 0) {
+                    time_receipt = `${Math.floor(differenceInDays)} days ago`
+                } else if (Math.floor(differenceInHours) > 0) {
+                    time_receipt = `${Math.floor(differenceInHours)} hours ago`
+                } else if (Math.floor(differenceInMinutes) > 0) {
+                    time_receipt = `${Math.floor(differenceInMinutes)} minutes ago`
+                } else if (Math.floor(differenceInSeconds) > 0) {
+                    time_receipt = `${Math.floor(differenceInSeconds)} seconds ago`
+                } else {
+                    time_receipt = `Just now`
+                }
+
+                receiptsObj[i].time_receipt = time_receipt
+            }
+            res.locals.notifications = receiptsObj
+            res.locals.numberReceipts = numberReceipts
+
             res.locals.user = req.session.user
             next()
         } else {
